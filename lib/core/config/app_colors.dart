67479119ffe511d6e636d1c74576679331260edd
kent0505 <otabekyusupov0550@gmail.com>
2024-09-21 13:41:29 +0500

    changed fonts

--- lib/core/config/app_colors.dart
+++ lib/core/config/app_colors.dart
@@ -20,5 +20,4 @@ class Fonts {
   static const String regular = 'SFR';
   static const String medium = 'SFM';
   static const String bold = 'SFB';
-  static const String extra = 'SFE';
 }
--- lib/core/widgets/texts/text_r.dart
+++ lib/core/widgets/texts/text_r.dart
@@ -97,35 +97,3 @@ class TextB extends StatelessWidget {
     );
   }
 }
-
-class TextE extends StatelessWidget {
-  const TextE(
-    this.title, {
-    super.key,
-    required this.fontSize,
-    this.color = AppColors.white,
-    this.textAlign,
-    this.maxLines,
-  });
-
-  final String title;
-  final double fontSize;
-  final Color color;
-  final TextAlign? textAlign;
-  final int? maxLines;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      title,
-      textAlign: textAlign,
-      maxLines: maxLines,
-      overflow: TextOverflow.ellipsis,
-      style: TextStyle(
-        color: color,
-        fontSize: fontSize,
-        fontFamily: Fonts.extra,
-      ),
-    );
-  }
-}
